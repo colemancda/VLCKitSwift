0ff1dbb77b5da31fb7bfec94979ae4f602ccbfcc
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-10-02 19:34:08 -0500

    Working on VLC

--- Sources/Configuration.swift
+++ Sources/Configuration.swift
@@ -8,57 +8,115 @@
 
 public extension Core {
     
-    public enum Configuration {
+    public struct Configuration {
         
-        case noColor
-        case noOsd
-        case noVideoTitleShow
-        case noStats
-        case noSnapshotPreview
-        case avcodecFast
-        case textRenderer(String)
-        case aviIndex(Int)
-        case playAndPause
-        case verbose(Int)
-        case noSoundOutKeep
-        case videoOut(String)
-        case extraintf(String)
-    }
-}
-
-extension Core.Configuration /* :RawRepresentable */ {
-    
-    public var rawValue: String {
+        public var name: Name
         
-        switch self {
+        public var isEnabled: Bool
+        
+        public var value: Value?
+        
+        public init(name: Name, isEnabled: Bool = true, value: Value? = nil) {
             
-        case noColor: return
-        case noOsd
-        case noVideoTitleShow
-        case noStats
-        case noSnapshotPreview
-        case avcodecFast
-        case textRenderer(String)
-        case aviIndex(Int)
-        case playAndPause
-        case verbose(Int)
-        case noSoundOutKeep
-        case videoOut(String)
-        case extraintf(String)
+            self.name = name
+            self.isEnabled = isEnabled
+            self.value = value
         }
     }
 }
 
-extension Core.Options {
+public extension Core.Configuration {
+    
+    public enum Value {
+        
+        case string(Swift.String)
+        case number(Int)
+    }
+}
+
+extension Core.Configuration.Value: RawRepresentable {
+    
+    public init(rawValue: String) {
+        
+        if let number = Int(rawValue) {
+            
+            self = .number(number)
+            
+        } else {
+            
+            self = .string(rawValue)
+        }
+    }
+    
+    public var rawValue: String {
+        
+        switch self {
+            case let .number(value): return "\(value)"
+            case let .string(value): return value
+        }
+    }
+}
+
+public extension Core.Configuration.Value {
+    
+    public var isEmpty: Bool {
+        
+        switch self {
+        case .none:
+            return true
+        case .string,
+             .number:
+            return false
+        }
+    }
+}
+
+public extension Core.Configuration {
+    
+    public enum Name: String {
+        
+        case color
+        case osd
+        case videoTitleShow = "video-title-show"
+        case stats
+        case snapshotPreview = "snapshot-preview"
+        case avcodecFast = "avcodec-fast"
+        case textRenderer = "text-renderer"
+        case aviIndex = "avi-index"
+        case playAndPause = "play-and-pause"
+        case verbose
+        case soundOutputKeep = "sout-keep"
+        case videoOutput = "vout"
+        case extraintf
+    }
+}
+
+extension Core.Configuration: RawRepresentable {
+    
+    public init?(rawValue: String) {
+        
+        // TODO: Implement parsing
+        fatalError()
+    }
+    
+    public var rawValue: String {
+        
+        return "--" + (isEnabled ? "" : "no-") + name.rawValue + (value == nil ? "" : "=\(value?.rawValue ?? "")")
+    }
+}
+
+extension Core.Configuration {
     
     public static var `default`: Parameters {
         
-        
+        #if os(iOS)
+            return iOS
+        #endif
     }
     
-    private static var iOS: Set<Option> {
+    private static var iOS: Set<Configuration> {
         
-        var parameters: Parameters = [
+        var parameters: Set<Configuration> = [
             .noColor,
             .noOsd,
             .noVideoTitleShow,
