27f572e0e496a8828e7ed87b8760d09f9cfe5cef
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-10-05 02:38:25 -0500

    Added Unit Test

--- Tests/VLCKitSwiftTests/VLCKitSwiftTests.swift
+++ Tests/VLCKitSwiftTests/VLCKitSwiftTests.swift
@@ -9,15 +9,59 @@
 import Foundation
 import XCTest
 import VLCKitSwift
+import UIKit
 
-class VLCKitSwiftTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        //// XCTAssertEqual(VLCKitSwift().text, "Hello, World!")
-    }
+final class VLCKitSwiftTests: XCTestCase {
     
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    func testPlayVideo() {
+        
+        let url = URL(string: "http://ring-mac-app-assets.s3.amazonaws.com/player_test/x.mp4")!
+        
+        let media = Media(url: url)!
+        
+        let mediaPlayer = Player(media: media)
+        
+        let expectation = self.expectation(description: "Video finished playing")
+        expectation.assertForOverFulfill = false
+        
+        // notification handler
+        func mediaPlayerStateChanged() {
+            
+            print("State changed to \(mediaPlayer.state)")
+            
+            switch mediaPlayer.state {
+                
+            case .ended:
+                
+                expectation.fulfill()
+                
+            case .error:
+                
+                XCTFail("An error ocurred")
+                
+            default:
+                
+                break
+            }
+        }
+        
+        // register for notifications
+        mediaPlayer.eventManager.register(event: .mediaPlayerMediaChanged, callback: mediaPlayerStateChanged)
+        mediaPlayer.eventManager.register(event: .mediaPlayerPlaying, callback: mediaPlayerStateChanged)
+        mediaPlayer.eventManager.register(event: .mediaPlayerPaused, callback: mediaPlayerStateChanged)
+        mediaPlayer.eventManager.register(event: .mediaPlayerEncounteredError, callback: mediaPlayerStateChanged)
+        mediaPlayer.eventManager.register(event: .mediaPlayerEndReached, callback: mediaPlayerStateChanged)
+        mediaPlayer.eventManager.register(event: .mediaPlayerStopped, callback: mediaPlayerStateChanged)
+        mediaPlayer.eventManager.register(event: .mediaPlayerOpening, callback: mediaPlayerStateChanged)
+        mediaPlayer.eventManager.register(event: .mediaPlayerBuffering, callback: mediaPlayerStateChanged)
+        
+        // set view
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+        mediaPlayer.drawable = .view(view)
+        
+        // start playing
+        mediaPlayer.play()
+        
+        waitForExpectations(timeout: 60)
+    }
 }
